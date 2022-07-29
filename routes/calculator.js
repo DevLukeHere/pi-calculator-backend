512951f3bd2f6d599d2a53efd44bfec62e21f56e
Luke Wong <lukewong93@gmail.com>
2022-07-29 15:09:27 +0800

    Add route endpoints

---
+++ routes/calculator.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const router = express.Router()
+
+// GET pi value
+router.get('/', (req, res) => {
+  res.json({message: 'GET pi value'})
+})
+
+// POST pi value
+router.post('/', (req, res) => {
+  res.json({message: 'POST pi value'})
+})
+
+// UPDATE pi value
+router.patch('/', (req, res) => {
+  res.json({message: 'UPDATE pi value'})
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 require('dotenv').config()
+
 const express = require('express')
+const calculatorRoutes = require('./routes/calculator')
 
 // express app
 const app = express()
 
 // middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
+
+// routes
+app.use('/api/calculator', calculatorRoutes)
 
 // listen for requests
 app.listen(process.env.PORT, () => {
