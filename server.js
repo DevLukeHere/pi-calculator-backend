506a117fc8de97d791619027e645162aa3543911
Luke Wong <lukewong93@gmail.com>
2022-07-29 15:24:34 +0800

    Install & connect to MongoDB

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const calculatorRoutes = require('./routes/calculator')
+const express = require("express");
+const mongoose = require("mongoose");
+const calculatorRoutes = require("./routes/calculator");
 
 // express app
-const app = express()
+const app = express();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.use('/api/calculator', calculatorRoutes)
+app.use("/api/calculator", calculatorRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-})
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db and listening on port", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
