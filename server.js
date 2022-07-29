7f7e3675f9300f286b29a30cfc5d0b8fced93e90
Luke Wong <lukewong93@gmail.com>
2022-07-29 13:00:30 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+
+// express app
+const app = express()
+
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log('listening on port', process.env.PORT)
+})
