f17e7114a43b4da15317073c16bf4d5f307f9b8d
Luke Wong <lukewong93@gmail.com>
2022-08-01 23:17:10 +0800

    Refactor code

--- controllers/piController.js
+++ controllers/piController.js
@@ -1,40 +1,11 @@
 const Pi = require("../models/piModel");
 const mongoose = require("mongoose");
 
-// get pi values
+// get pi value
 const getPiValues = async (req, res) => {
   const piValues = await Pi.find({}).sort({ createdAt: -1 });
 
   res.status(200).json(piValues);
-};
-
-// get single pi value
-const getPiValue = async (req, res) => {
-  const { id } = req.params;
-
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "No such value" });
-  }
-
-  const pi = await Pi.findById(id);
-
-  if (!pi) {
-    return res.status(404).json({ error: "No such value" });
-  }
-
-  res.status(200).json(pi);
-};
-
-// create pi value
-const createPiValue = async (req, res) => {
-  const { pi } = req.body;
-
-  try {
-    const piValue = await Pi.create({ pi });
-    res.status(200).json(piValue);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
 };
 
 // update pi value
--- routes/calculator.js
+++ routes/calculator.js
@@ -1,17 +1,9 @@
 const express = require("express");
-const {
-  createPiValue,
-  getPiValues,
-  getPiValue,
-  updatePiValue,
-} = require("../controllers/piController");
+const { getPiValues, updatePiValue } = require("../controllers/piController");
 const router = express.Router();
 
 // GET pi value
 router.get("/", getPiValues);
-
-// POST pi value
-router.post("/:id", createPiValue);
 
 // UPDATE pi value
 router.patch("/:id", updatePiValue);
