f3fe734814f5d56f63a1d560101cc039c8e3b517
Luke Wong <lukewong93@gmail.com>
2022-08-03 10:54:01 +0800

    Add comments

--- controllers/piController.js
+++ controllers/piController.js
@@ -1,14 +1,14 @@
 const Pi = require("../models/piModel");
 const mongoose = require("mongoose");
 
-// get pi value
+// get Pi value
 const getPiValues = async (req, res) => {
   const piValues = await Pi.find({}).sort({ createdAt: -1 });
 
   res.status(200).json(piValues);
 };
 
-// update pi value
+// update Pi value
 const updatePiValue = async (req, res) => {
   const { id } = req.params;
 
@@ -20,7 +20,7 @@ const updatePiValue = async (req, res) => {
   let final = 0;
   let precision = req.body.precision + 1;
 
-  // Algorithm to calculate the value of Pi
+  // algorithm to calculate the value of Pi
   for (let n = 0; n < 1000; n++) {
     let numerator = Math.pow(-1, n);
     let denominator = 2 * n + 1;
@@ -41,6 +41,7 @@ const updatePiValue = async (req, res) => {
     return res.status(400).json({ error: "No such value" });
   }
 
+  // return updated value of Pi to frontend
   res.status(200).json({ _id: id, precision: precision, pi: final });
 };
 
