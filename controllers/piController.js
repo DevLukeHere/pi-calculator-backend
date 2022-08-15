1941b4c8a039f498ae3d6d70b739278a8c09f3f3
Luke Wong <lukewong93@gmail.com>
2022-08-15 11:54:21 +0800

    Update piController.js

--- controllers/piController.js
+++ controllers/piController.js
@@ -12,15 +12,16 @@ const getPiValues = async (req, res) => {
 const updatePiValue = async (req, res) => {
   const { id } = req.params;
 
+  // validate if object ID exists in database
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "No such value" });
   }
 
+  // start of algorithm to calculate the value of Pi using the Gregory-Liebniz series
   let total = 0;
   let final = 0;
   let precision = req.body.precision + 1;
 
-  // algorithm to calculate the value of Pi using the Gregory-Liebniz series
   for (let n = 0; n < 1000000; n++) {
     let numerator = Math.pow(-1, n);
     let denominator = 2 * n + 1;
@@ -29,7 +30,24 @@ const updatePiValue = async (req, res) => {
   }
 
   final = parseFloat((4 * total).toFixed(precision));
+  // end of algorithm
 
+  // let i = 1n;
+  // let x = 3n * 10n ** 1020n;
+  // let pi_final = x;
+  // let final_value = 0;
+
+  // while (x > 0) {
+  //   x = (x * i) / ((i + 1n) * 4n);
+  //   pi_final += x / (i + 2n);
+  //   i += 2n;
+  // }
+
+  // final_value = pi_final / 10n ** 20n;
+
+  // console.log("final_value:", final_value);
+
+  // find and update object based on calculated Pi value
   const pi = await Pi.findOneAndUpdate({
     _id: id,
     ...req.body,
@@ -37,6 +55,7 @@ const updatePiValue = async (req, res) => {
     pi: final,
   });
 
+  // check if Pi value exists in database
   if (!pi) {
     return res.status(400).json({ error: "No such value" });
   }
